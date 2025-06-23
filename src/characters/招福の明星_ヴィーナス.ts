4747e37dc8579a96a0f7e8034c7a4f83d1f7beed
lauirf <592184299@qq.com>
2025-06-23 22:02:37 +0800

    Refactor 招福の明星_ヴィーナス skill methods to use battle system for EX gain and improve code formatting

--- src/characters/招福の明星_ヴィーナス.ts
+++ src/characters/招福の明星_ヴィーナス.ts
@@ -1,21 +1,20 @@
-import { CharacterBase } from "../types/CharacterBase";
+import { CharacterBase } from '../types/CharacterBase';
 
 export class 招福の明星_ヴィーナス extends CharacterBase {
-
-    useSkill1(team: CharacterBase[]): void {
-        if(this.isCharged){
-            this.eventEmitter.emit("onExIncrease", 100); // 触发 EX 增加事件
-        }
-        for (const c of team) {
-            c.ctForward(7); // 给队友充能
-        }
+  useSkill1(team: CharacterBase[]): void {
+    if (this.isCharged) {
+      this.battleSystem.addEx(100);
     }
-
-    useSkill2(team: CharacterBase[]): void {
-        for (const c of team) {
-            if (c.data.faction === "人間" || c.data.element === "水") {
-                c.ctForward(7); // 给队友充能
-            }
-        }
+    for (const c of team) {
+      c.ctForward(7); // 给队友充能
     }
+  }
+
+  useSkill2(team: CharacterBase[]): void {
+    for (const c of team) {
+      if (c.data.faction === '人間' || c.data.element === '水') {
+        c.ctForward(7); // 给队友充能
+      }
+    }
+  }
 }
