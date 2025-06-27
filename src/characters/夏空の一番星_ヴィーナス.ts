f6e0d64dd55865a521b3b822034ba0d993287f25
lauirf <592184299@qq.com>
2025-06-28 00:10:42 +0800

    Refactor skill1 implementation for 夏空の一番星_ヴィーナス to use buff system and simplify cooldown tracking(closes #9)

--- src/characters/夏空の一番星_ヴィーナス.ts
+++ src/characters/夏空の一番星_ヴィーナス.ts
@@ -2,18 +2,15 @@ import emitter from '../eventBus';
 import { CharacterBase } from '../types/CharacterBase';
 
 export class 夏空の一番星_ヴィーナス extends CharacterBase {
-  private skill1Active: boolean = false; // 跟踪 Skill1 是否激活
-  private skill1RemainingCt: number = 0; // 跟踪 Skill1 的剩余持续时间
-
   useSkill1(team: CharacterBase[]): void {
-    console.log(
-      `夏空の一番星_ヴィーナス useSkill1 in class_夏空の一番星_ヴィーナス`
-    );
-    this.skill1Active = true;
-    this.skill1RemainingCt = 25; // 设置持续时间为 25 CT
+    this.setBuff({
+      target: 'ex_up',
+      name: '夏空の一番星_ヴィーナス',
+      val: 60,
+      duration: 25,
+    });
     emitter.emit('ex-up-changed', this.data.name);
     for (const c of team) {
-      console.log(`${c.data.name} Forward(7)`);
       c.ctForward(7);
     }
   }
@@ -27,16 +24,12 @@ export class 夏空の一番星_ヴィーナス extends CharacterBase {
   }
 
   getEx_up(): number {
-    return super.getEx_up() + (this.skill1Active ? 60 : 0);
-  }
-
-  updateCT(): void {
-    super.updateCT();
-    if (this.skill1Active) {
-      this.skill1RemainingCt -= 1;
-      if (this.skill1RemainingCt <= 0) {
-        this.skill1Active = false; // 技能效果结束
+    let buffExUp = 0;
+    for (const buff of this.buffs) {
+      if (buff.target === 'ex_up') {
+        buffExUp += buff.val;
       }
     }
+    return super.getEx_up() + buffExUp;
   }
 }
