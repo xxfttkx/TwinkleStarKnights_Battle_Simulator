b5b3ff6eca89c6dfbd38970dc6bc3f0b9ee5875d
lauirf <592184299@qq.com>
2025-07-02 14:53:36 +0800

    add 煌炎の超天使_アナ_煌炎 skills and buffs

--- src/characters/煌炎の超天使アナ《煌炎》.ts
+++ src/characters/煌炎の超天使アナ《煌炎》.ts
@@ -3,15 +3,22 @@ import { CharacterBase } from '../types/CharacterBase';
 export class 煌炎の超天使_アナ_煌炎 extends CharacterBase {
   useSkill1(team: CharacterBase[]): void {
     for (const c of team) {
-      if (c.isFaction('神族')||c.isElement('炎')) {
+      if (c.isFaction('神族') || c.isElement('炎')) {
         c.ctForward(4);
+        // 行動CT20％短縮（5CT）
+        c.setBuff({
+          target: 'ct',
+          name: '煌炎の超天使_アナ_煌炎_スキル1',
+          val: 0.2,
+          duration: 5,
+        });
       }
     }
   }
 
   useSkill2(team: CharacterBase[]): void {
     for (const c of team) {
-      if (c.isFaction('神族')||c.isElement('炎')) {
+      if (c.isFaction('神族') || c.isElement('炎')) {
         c.ctForward(7);
       }
     }
