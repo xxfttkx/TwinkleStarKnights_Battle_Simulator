df38012e0f59a93ad4a9b620073a84cbf3540116
lauirf <592184299@qq.com>
2025-07-07 23:49:51 +0800

    add function to scrape character data from webpage and save as JSON

---
+++ src/data/main.py
@@ -0,0 +1,59 @@
+from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def table_to_json(url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = None
+    tables = soup.find_all("table")
+    print(f"Found {len(tables)} tables on the page.")
+    for table in tables:
+        first_row = table.find("tr")
+        first_cell = first_row.find(['td', 'th']) if first_row else None
+        if first_cell:
+            text = first_cell.get_text(strip=True)
+            # 这就是角色表
+            if(text=='No'):
+                table = table
+                break
+    
+    print(f"table found: {table is not None}")
+    # 提取表头
+    headers = [th.get_text(strip=True) for th in table.find_all('tr')[0].find_all(['th', 'td'])]
+    print("Headers:", headers)
+    data = []
+    for row in table.find_all('tr')[1:]:
+        cells = row.find_all(['td', 'th'])
+        if len(cells) != len(headers):
+            continue
+        item = {}
+        for i in range(len(headers)):
+            cell = cells[i]
+            if headers[i] == "画像":
+                img_tag = cell.find("img")
+                if img_tag:
+                    image_url = img_tag.get("data-src") or img_tag.get("src")
+                    item[headers[i]] = urljoin(url, image_url)
+                else:
+                    item[headers[i]] = ""
+            else:
+                item[headers[i]] = cell.get_text(strip=True)
+        data.append(item)
+
+
+    return data
+
+print("start")
+# 示例使用
+url = "https://twinklestarknights.wikiru.jp/?キャラクター一覧"
+json_data = table_to_json(url)
+
+
+
+with open("characters_all.json", "w", encoding="utf-8") as f:
+    json.dump(json_data, f, ensure_ascii=False, indent=2)
+
